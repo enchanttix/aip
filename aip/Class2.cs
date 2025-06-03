a5c68513a11e82b11505c190b96fb963f5332612
Smerdova Liza <smerdova_vz@mail.ru>
2025-06-03 18:05:01 +0300

    вторая контрольная

---
+++ aip/Class2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aip
+{
+    public class Class2
+    {
+        public static int[] Arr(int i)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[i];
+            for (int j = 0; j < arr.Length; j++)
+            {
+                arr[j] = rnd.Next(10);
+            }
+            return arr;
+        }
+
+        public static string zz(int[] arr)
+        {
+            int sum=0;
+            int ind = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]%2==0)
+                {
+                    sum += arr[i];
+                }
+                else
+                {
+                    ind++;
+                }
+            }
+            return "сумма четных элементов = " + sum + " количество нечетных элементов = " + ind;
+        }
+    }
+}
--- aip/Program.cs
+++ aip/Program.cs
@@ -10,12 +10,22 @@ namespace aip
     {
         static void Main(string[] args)
         {
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //string month = aip.Class1.Z1(a);
+            //Console.WriteLine(month);
+            //double a1 = Convert.ToDouble(Console.ReadLine());
+            //double rez = Class1.Z2(a1);
+            //Console.WriteLine(rez);
+            //Console.ReadLine();
+
             int a = Convert.ToInt32(Console.ReadLine());
-            string month = aip.Class1.Z1(a);
-            Console.WriteLine(month);
-            double a1 = Convert.ToDouble(Console.ReadLine());
-            double rez = Class1.Z2(a1);
-            Console.WriteLine(rez);
+            int[] arr = aip.Class2.Arr(a);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(Class2.zz(arr));
             Console.ReadLine();
         }
     }
